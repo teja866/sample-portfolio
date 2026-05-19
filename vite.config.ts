b6064bebf98c2742e8c76eefa334cadd869eef57
B V B S N D S GNANA TEJA <gnanateja234@gmail.com>
2026-05-19 18:02:03 +0530

    Revert base URL to use default GitHub Pages free domain

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/',
+  base: '/sample-portfolio/',
   plugins: [react()],
   resolve: {
     alias: {
